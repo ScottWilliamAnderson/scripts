7c2dabe793be8ed7917550d4df2dccc0a1947651
ScottWilliamAnderson <anderson.scott.william@gmail.com>
2024-11-28 23:54:06 +0000

    Add GitHub Copilot setup function to PowerShell profile

--- powershell-profile/profile.ps1
+++ powershell-profile/profile.ps1
@@ -43,6 +43,40 @@ if ($env:ENABLE_PROFILE_LOGGING -eq "true") {
     $enableLogging = $true
 }
 
+function copilot-setup {
+    <#
+    .SYNOPSIS
+        Sets up the github copilot profile.
+    .PARAMETER Force
+        Aliases: -f
+        Forces regeneration of the copilot profile even if it exists.
+    .OUTPUTS
+        None
+    #>
+    param (
+        [Alias("f")]
+        [switch]$Force
+    )
+
+    # Run the command
+    gh copilot --version > $null
+
+    # Check the exit code
+    if ($LASTEXITCODE -eq 0) {
+        # if $GH_PROFILE is not set, set it to the default value
+        if (-not $GH_COPILOT_PROFILE) {
+            $GH_COPILOT_PROFILE = Join-Path -Path $(Split-Path -Path $PROFILE -Parent) -ChildPath "gh-copilot.ps1"
+        }
+        if ((-not (Test-Path $GH_COPILOT_PROFILE)) -or $Force) {
+            gh copilot alias -- pwsh | Out-File ( New-Item -Path $GH_COPILOT_PROFILE -Force )
+        }
+        . $GH_COPILOT_PROFILE
+
+    } else {
+        Write-Output "Github Copilot is not installed or there was an error."
+    }
+}
+
 # List of commands to run and their descriptions
 $commands = @(
     @{ Command = { Import-Module $env:ChocolateyInstall\helpers\chocolateyProfile.psm1 }; Description = "Chocolatey Profile Import" },
